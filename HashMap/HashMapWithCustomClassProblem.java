6511b2b70c6f498bdda506c7776209a589174a92
Abhishek Dhiman <dhimana862@gmail.com>
2025-07-24 02:21:21 +0530

    updated comments for better understanding

--- HashMap/HashMapWithCustomClassProblem.java
+++ HashMap/HashMapWithCustomClassProblem.java
@@ -6,39 +6,64 @@ public class HashMapWithCustomClassProblem {
 
     public static void main(String[] args) {
 
+        // WHY: We're using a HashMap with a custom class (Student) as keys.
+        // This is to test whether two objects with the same content are treated as equal keys.
         HashMap<Student, String> studentHashMap = new HashMap<>();
 
+        // Creating student objects with some duplicate data (s1 and s4 are identical in content)
         Student s1 = new Student("Neha", 101, "Designer");
         Student s2 = new Student("Rahul", 322, "Engineer");
         Student s3 = new Student("Sunil", 782, "HR");
-        Student s4 = new Student("Neha", 101, "Designer" );
 
-        studentHashMap.put( s1, "Working");
-        studentHashMap.put( s2,"OOO");
-        studentHashMap.put( s3, "Working");
-        studentHashMap.put( s4, "Leave");
+        // s4 has the same data as s1 but is a different object in memory (new keyword)
+        Student s4 = new Student("Neha", 101, "Designer");
 
-        //System.out.println(studentHashMap);
+        // Adding entries to the HashMap
+        studentHashMap.put(s1, "Working");  // Adds entry for s1
+        studentHashMap.put(s2, "OOO");      // Adds entry for s2
+        studentHashMap.put(s3, "Working");  // Adds entry for s3
+
+        // Adds s4 as a new key, even though it looks identical to s1.
+        // WHY: Because Student does NOT override equals() and hashCode(),
+        // HashMap uses the default implementation (object reference comparison),
+        // treating s4 as a DIFFERENT key from s1.
+        studentHashMap.put(s4, "Leave");
+
+        // Expected size should be 3, but output will be 4 due to lack of equals/hashCode
         System.out.println(studentHashMap.size());
+
+        // Print values for both s1 and s4
+        // They are different keys, so both entries exist independently
         System.out.println("s1 value :" + s1.getName() + " " + studentHashMap.get(s1));
         System.out.println("s4 value :" + s4.getName() + " " + studentHashMap.get(s4));
 
-        System.out.println("Size should be 3 not 4 and Neha status should change from working to leave beacuse here we are using new keyword and " +
-                "so it will create a new memory and hashcode will generate different So here they are two different students not one." +
-                "how to overcome this???????");
+        // WHY THIS MATTERS:
+        // Even though s1 and s4 have the same content, they are treated as different keys
+        // This breaks expected behavior when using custom objects in maps or sets.
 
-
-
+        // Educating the user on the issue and hinting at the solution
+        System.out.println("Size should be 3 not 4 and Neha status should change from working to leave because here we are using 'new' keyword " +
+                "which creates new memory references. Since the class doesn't override equals() and hashCode(), " +
+                "the HashMap treats s1 and s4 as different keys. " +
+                "\nHOW TO OVERCOME THIS??????? => Override equals() and hashCode() methods in Student class.");
     }
 }
 
+// Custom class used as key in HashMap
 class Student {
 
     private int id;
     private String name;
     private String job;
 
+    // Constructor
+    public Student(String name, int id, String job){
+        this.name = name;
+        this.id = id;
+        this.job = job;
+    }
 
+    // Getter methods
     public int getId() {
         return id;
     }
@@ -47,12 +72,7 @@ class Student {
         return name;
     }
 
-    public Student(String name, int id, String job){
-        this.name = name;
-        this.id = id;
-        this.job = job;
-    }
-
+    // toString override to help print student details clearly
     @Override
     public String toString() {
         return "Student{" +
@@ -61,4 +81,16 @@ class Student {
                 ", job='" + job + '\'' +
                 '}';
     }
+
+    /*
+     * MISSING:
+     * Without overriding equals() and hashCode(), the HashMap uses default Object methods,
+     * which compare memory references (not logical values).
+     *
+     * To fix this problem:
+     * 1. Override equals() to compare fields like id and name.
+     * 2. Override hashCode() so that logically equal objects go to the same bucket.
+     *
+     * See: HashMapWithCustomClassSolution.java for a working example.
+     */
 }
--- HashMap/HashMapWithCustomClassSolution.java
+++ HashMap/HashMapWithCustomClassSolution.java
@@ -7,38 +7,61 @@ public class HashMapWithCustomClassSolution {
 
     public static void main(String[] args) {
 
+        // WHY: We're creating a HashMap where the key is a custom object (StudentSolution).
+        // This is to understand how HashMap behaves when custom objects are used as keys.
         HashMap<StudentSolution, String> studentHashMap = new HashMap<>();
 
+        // WHY: Creating different student objects to test key uniqueness and equality
         StudentSolution s1 = new StudentSolution("Neha", 101, "Designer");
         StudentSolution s2 = new StudentSolution("Rahul", 322, "Engineer");
         StudentSolution s3 = new StudentSolution("Sunil", 782, "HR");
-        StudentSolution s4 = new StudentSolution("Neha", 101, "Designer" );
 
-        studentHashMap.put( s1, "Working");
-        studentHashMap.put( s2,"OOO");
-        studentHashMap.put( s3, "Working");
-        studentHashMap.put( s4, "Leave");
+        // WHY: s4 is deliberately made with the same data as s1 to test if HashMap
+        // treats them as the same key (i.e., whether s4 will overwrite s1's entry)
+        StudentSolution s4 = new StudentSolution("Neha", 101, "Designer");
 
-        //System.out.println(studentHashMap);
+        // WHY: Adding student entries to the HashMap.
+        // This tests whether custom keys behave like expected (based on equals & hashCode)
+        studentHashMap.put(s1, "Working");  // Adding s1
+        studentHashMap.put(s2, "OOO");      // Adding s2
+        studentHashMap.put(s3, "Working");  // Adding s3
+
+        // WHY: Adding s4 will only overwrite s1's value **if** equals() and hashCode() are implemented correctly.
+        // If not, it will insert as a separate key.
+        studentHashMap.put(s4, "Leave");
+
+        // WHY: Printing size tells us if the map treated s1 and s4 as one key or two.
+        // If size = 3, then s1 and s4 are considered equal keys (overwritten).
         System.out.println(studentHashMap.size());
+
+        // WHY: Verifying if we get the updated value ("Leave") from both s1 and s4 as keys
         System.out.println("s1 value :" + s1.getName() + " " + studentHashMap.get(s1));
         System.out.println("s4 value :" + s4.getName() + " " + studentHashMap.get(s4));
 
+        // WHY: Redundant prints to confirm consistency of retrieval
         System.out.println("s1 value :" + s1.getName() + " " + studentHashMap.get(s1));
         System.out.println("s4 value :" + s4.getName() + " " + studentHashMap.get(s4));
-
-
-
     }
 }
 
+/**
+ * Custom class StudentSolution to be used as a key in HashMap.
+ * WHY: To test how HashMap works with objects we define ourselves.
+ */
 class StudentSolution {
 
     private int id;
     private String name;
     private String job;
 
+    // Constructor
+    public StudentSolution(String name, int id, String job){
+        this.name = name;
+        this.id = id;
+        this.job = job;
+    }
 
+    // Getter methods
     public int getId() {
         return id;
     }
@@ -47,40 +70,44 @@ class StudentSolution {
         return name;
     }
 
-    public StudentSolution(String name, int id, String job){
-        this.name = name;
-        this.id = id;
-        this.job = job;
-    }
-
+    /**
+     * WHY: hashCode() is used by HashMap to place the key in a bucket.
+     * We override it so that two logically equal StudentSolution objects
+     * go to the same bucket and can be compared via equals().
+     */
     @Override
     public int hashCode() {
+        // Using name and id (but not job) to compute hash
         return Objects.hash(name, id);
     }
 
+    /**
+     * WHY: equals() checks if two StudentSolution objects are logically equal.
+     * If not overridden, default equals checks object references (i.e., memory address).
+     * Overriding it allows the HashMap to treat s1 and s4 as the same key.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj){
-            return true;
+            return true;  // Same memory reference
         }
 
-        if (obj == null){
-            return false;
-        }
-
-        if(getClass() != obj.getClass()){
-            return false;
+        if (obj == null || getClass() != obj.getClass()){
+            return false; // Different type or null
         }
 
         StudentSolution other = (StudentSolution) obj;
 
+        // All fields must match for objects to be considered equal
         return id == other.getId()
                 && Objects.equals(name, other.name)
                 && Objects.equals(job, other.job);
-
-
     }
 
+    /**
+     * WHY: Overriding toString() helps print Student objects clearly.
+     * Useful for debugging and logging.
+     */
     @Override
     public String toString() {
         return "Student{" +
